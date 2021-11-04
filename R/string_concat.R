bc3e3e51a99a12b171a43d3f66e3d55b251346e1
adam <adamleejohnson@gmail.com>
2021-11-03 23:17:55 -0400

    No export

--- R/string_concat.R
+++ R/string_concat.R
@@ -1,10 +1,9 @@
 #' String concatenation
 #' @param x string x
 #' @param y string y
-#' @export
 "%+%" <- function(x, y) {
   if (is.character(x) & is.character(y)) {
-    return(paste(x, y, sep = ""))
+    return(paste0(x, y))
   } else {
     .Primitive("+")(x, y)
   }
