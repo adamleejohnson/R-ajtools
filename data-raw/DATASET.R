039339092f0e922d17835802ba879a309a66c766
Adam Johnson <adam@Adams-MacBook-Air-M3.local>
2026-01-29 13:28:13 -0500

    Updates

--- data-raw/DATASET.R
+++
@@ -1,9 +0,0 @@
-## code to prepare `DATASET` dataset goes here
-
-samples_dir <- "~/Documents/+RESEARCH/MalhotraLab/Archives.nosync/1000G/1000G_population_samples/"
-samples_files <- list.files(path = samples_dir, pattern = "*.txt")
-samples_names <- xfun::sans_ext(samples_files)
-
-population_samples <- sapply(samples_names, function(n) readLines(paste0(samples_dir, n, ".txt")))
-
-usethis::use_data(population_samples, overwrite = TRUE, internal = T, compress = "gzip")
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(ajtools)
-
-test_check("ajtools")
